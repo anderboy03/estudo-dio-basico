58be7c12021ec5a70a73f2a5e4903f7086678675
Anderfoxx <anderfox1988@gmail.com>
2024-05-09 19:06:52 -0300

    projeto-conta

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,5 +1,38 @@
+
+import java.util.Scanner;
+
 public class contaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Agora, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer após ler o número da conta
+
+        System.out.println("Digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por último, digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+ 
